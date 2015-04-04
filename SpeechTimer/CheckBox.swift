4012981f8cc8bd4ad20267dff7f57f447e74bfbb
Anson Long-Seabra <anson.longseabra@gmail.com>
2015-04-04 12:30:43 -0500

    Button class added, one button added to setup (dead)

---
+++ SpeechTimer/CheckBox.swift
@@ -0,0 +1,59 @@
+//
+//  CheckBox.swift
+//  SpeechTimer
+//
+//  Created by Anson Long-Seabra on 4/4/15.
+//  Copyright (c) 2015 Anson Long-Seabra. All rights reserved.
+//
+
+import UIKit
+
+
+//A simple checkbox button
+class CheckBox: UIButton {
+
+
+    
+    var isChecked : Bool {
+        didSet{
+            if (isChecked == true) {
+                self.setImage(checkedImage, forState: .Normal);
+            } else {
+                self.setImage(uncheckedImage, forState: .Normal);
+            }
+        }
+    };
+    
+    lazy var checkedImage : UIImage = UIImage(named: "checked")!;
+    
+    lazy var uncheckedImage : UIImage = UIImage(named: "unchecked")!;
+    
+    override func awakeFromNib() {
+        
+        self.isChecked = false;
+        
+        self.setImage(uncheckedImage, forState: .Normal);
+        
+        self.addTarget(self, action: "buttonClicked", forControlEvents: UIControlEvents.TouchUpInside);
+
+    }
+    
+    func buttonClicked () {
+    
+    self.isChecked = !self.isChecked;
+    
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        self.isChecked = false;
+        super.init(coder: aDecoder)
+        self.setImage(uncheckedImage, forState: .Normal);
+        
+    }
+
+    
+    
+
+    
+
+}
