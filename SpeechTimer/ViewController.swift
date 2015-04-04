87ca499c8fb7109433043ab7f705e88c90083c26
Anson Long-Seabra <anson.longseabra@gmail.com>
2015-04-03 22:32:59 -0500

    Initial Commit

---
+++ SpeechTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeechTimer
+//
+//  Created by Anson Long-Seabra on 4/3/15.
+//  Copyright (c) 2015 Anson Long-Seabra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
