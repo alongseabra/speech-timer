90d406fd450f9ca88350787bb833b4499fa137fd
Anson Long-Seabra <anson.longseabra@gmail.com>
2015-04-04 11:09:48 -0500

    Added empty setup screen with working back button

--- SpeechTimer/SetupViewController.swift
+++ SpeechTimer/SetupViewController.swift
@@ -1,29 +1,28 @@
 //
-//  ViewController.swift
+//  SetupViewController.swift
 //  SpeechTimer
 //
-//  Created by Anson Long-Seabra on 4/3/15.
+//  Created by Anson Long-Seabra on 4/4/15.
 //  Copyright (c) 2015 Anson Long-Seabra. All rights reserved.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController {
+class SetupViewController: UIViewController {
+    
 
-    @IBOutlet weak var LogoLabel: UILabel!
-    @IBOutlet weak var TimeMeButton: UIButton!
-    @IBOutlet weak var MoreInfoButton: UIButton!
+    @IBOutlet weak var BackButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
