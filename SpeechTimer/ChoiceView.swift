e0adf4d4f29d39c6fcdcd03c91347dad30e8c928
Anson Long-Seabra <anson.longseabra@gmail.com>
2015-04-06 21:24:49 -0500

    ChoiceView now has correct positioning for all subviews

--- SpeechTimer/ChoiceView.swift
+++ SpeechTimer/ChoiceView.swift
@@ -8,28 +8,50 @@
 
 import UIKit
 
-
+//Represents a choice, which is a CheckBox and UILabel
 class ChoiceView: UIView {
     
-    var checkBoxWidth : CGFloat;
+    //The percentage of the total width that the label takes up
+    let labelRatio : CGFloat = 0.75;
     
     var checkBox : CheckBox;
+    var checkBoxContainerView : PositioningView;
+    var checkBoxWidth : CGFloat;
+
+    var label : UILabel;
+    var labelWidth : CGFloat;
+    var labelHeight : CGFloat;
+    
     
     override init(frame: CGRect) {
         
-
-        self.checkBoxWidth = frame.size.width / 8.0;
+        //setup the checkbox
+        self.checkBoxWidth = frame.size.height *  0.25;
+        self.checkBox = CheckBox(frame: CGRectMake(frame.size.width / 8.0, frame.size.height / 3.0,
+                                                    checkBoxWidth, checkBoxWidth));
+        self.checkBoxContainerView = PositioningView(frame: CGRectMake(0,
+                                                    frame.size.height / 3,
+                                                    frame.size.width / 4,
+                                                    frame.size.height / 3));
+        self.checkBoxContainerView.addSubview(self.checkBox);
+        self.checkBoxContainerView.centerSubview();
         
-        self.checkBox = CheckBox(frame: CGRectMake(0, 0, checkBoxWidth, checkBoxWidth));
+        //setup the label
+        self.labelWidth = frame.size.width * self.labelRatio;
+        self.labelHeight = frame.size.height;
+        self.label = UILabel(frame: CGRectMake(frame.size.width * 0.25, 0, labelWidth, labelHeight));
+        self.label.backgroundColor = UIColor.redColor();
         
         super.init(frame: frame);
-
-        self.addSubview(self.checkBox);
+    
+        self.addSubview(self.checkBoxContainerView);
+        self.addSubview(self.label);
         
         self.backgroundColor = UIColor.blueColor();
         
-
     }
+    
+
 
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
---
+++ SpeechTimer/PositioningView.swift
@@ -0,0 +1,29 @@
+//
+//  PositioningView.swift
+//  SpeechTimer
+//
+//  Created by Anson Long-Seabra on 4/6/15.
+//  Copyright (c) 2015 Anson Long-Seabra. All rights reserved.
+//
+
+import UIKit
+
+
+//A class that can position its subview
+class PositioningView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame);
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func centerSubview()
+    {
+        var sub : UIView = self.subviews[0] as! UIView;
+        sub.center = CGPointMake(self.frame.size.width / 2, self.frame.size.height / 2);
+    }
+
+}
