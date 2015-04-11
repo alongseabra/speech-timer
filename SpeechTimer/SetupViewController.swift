d581ddbfcd144d74fed6abb5e183f53e0dc8676a
Anson Long-Seabra <anson.longseabra@gmail.com>
2015-04-11 13:37:08 -0500

    Added a UIPickerView to SetupViewController

--- SpeechTimer/SetupViewController.swift
+++ SpeechTimer/SetupViewController.swift
@@ -9,7 +9,7 @@
 
 import UIKit
 
-class SetupViewController: UIViewController {
+class SetupViewController: UIViewController, UIPickerViewDelegate {
     
 
     @IBOutlet weak var BackButton: UIButton!
@@ -21,6 +21,12 @@ class SetupViewController: UIViewController {
     var automaticChoice : ChoiceView?;
     var choiceContainerView : ChoiceContainerView?;
     
+    var finishTimeLabelContainerView : UIView?;
+    var finishTimeLabel : UILabel?;
+    var finishTimePicker : UIPickerView?;
+    
+    var nums : [String] = ["5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"];
+
     
     override func viewDidLoad() {
         
@@ -33,10 +39,31 @@ class SetupViewController: UIViewController {
 
         self.choiceContainerView?.addChoiceView(self.manualChoice!);
         self.choiceContainerView?.addChoiceView(self.automaticChoice!);
+    
+        self.manualChoice?.label.text = "Manual";
+        self.automaticChoice?.label.text = "Automatic";
         self.view.addSubview(self.choiceContainerView!);
-
+        
+        self.finishTimePicker = UIPickerView(frame: CGRectMake(0, 300, 50, 50));
+        self.finishTimePicker?.delegate = self;
+        self.view.addSubview(self.finishTimePicker!);
         
 
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView!) -> Int {
+        return 1;
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+       
+        return self.nums.count;
+        
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        
+        return self.nums[row];
     }
     
     override func didReceiveMemoryWarning() {
---
+++ SpeechTimer/TimerViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TimerViewController.swift
+//  SpeechTimer
+//
+//  Created by Anson Long-Seabra on 4/11/15.
+//  Copyright (c) 2015 Anson Long-Seabra. All rights reserved.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+
+}
